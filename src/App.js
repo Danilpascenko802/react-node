278ff3356b37ce2a6e00c9d05bfea53dc35426a5
Danilpaschenko802 <Danilpaschenko802@gmail.com>
2022-10-05 21:17:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import firstF from "./firstfunction";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div class="header">
+        <div class="navigation_box">
+          <ul class="navigation_list">
+            <li id="header_logo" class="navigation_pages"></li>
+            <li class="navigation_pages">
+              <a class="nav_pages_link">Заказчики</a>
+            </li>
+            <li class="navigation_pages">
+              <a class="nav_pages_link">Мастера</a>
+            </li>
+            <li class="navigation_pages">
+              <a class="nav_pages_link">Контакты</a>
+            </li>
+          </ul>
+          <button class="entrance_btn">
+            <a id="entrance_btn_link">Войти</a>
+          </button>
+        </div>
+        <div class="search_box">
+          <input
+            class="header_search_bar"
+            type="text"
+            placeholder="  Найди то что ищешь"
+          />
+          <button class="search_btn"></button>
+        </div>
+      </div>
+      <div id="main_box">
+        <ul class="registration_type_list">
+          <li class="registration_type_item">
+            <a href="" class="registration_type_item_link">
+              Ищу мастера
+            </a>
+          </li>
+          <li class="registration_type_item">
+            <a href="" class="registration_type_item_link">
+              Я мастер
+            </a>
+          </li>
+        </ul>
+        <div id="registration_title" class="text">
+          Registration
+        </div>
+        <div id="registration_box">
+          <form id="form_box" action="/tutorial/action.html">
+            <input
+              class="regisrtation_form"
+              id="regisrtation_form_email"
+              type="text"
+              name="firstname"
+              placeholder="Почта"
+            />
+            <input
+              class="regisrtation_form"
+              id="regisrtation_form_password"
+              type="text"
+              name="lastname"
+              placeholder="Пароль"
+            />
+          </form>
+          <div id="showPass_box">
+            <input type="checkbox" id="showPass_box_checkbox" />{" "}
+            <div id="showPass_box_text">Показать пароль</div>
+          </div>
+          <button id="submit_bttn">Enter</button>
+        </div>
+      </div>
+    </body>
   );
 }
 
