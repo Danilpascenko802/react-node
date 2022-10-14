b34dda59c7d55aaf6343ed9896113ec6acb72403
Danilpaschenko802 <Danilpaschenko802@gmail.com>
2022-10-14 14:52:24 +0200

    several pages App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
 import Header from "./components/header";
-<<<<<<< HEAD
-/*import Mainpage from "./components/mainPage";*/
-import { Outlet } from "react-router-dom";
-/*import Registration from "./components/registration"; */
-/*import Masters from "./components/masters";*/
-/*import { BrowserRouter as Router, Routes, Route } from "react-router-dom";*/
-/*import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";*/
-/*import Contacts from "./components/contacts";*/
-=======
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Masters from './pages/Masters';
 import { Routes, Route } from 'react-router-dom';
 
->>>>>>> origin/fix-router
+
 function App() {
   return (
     <>
