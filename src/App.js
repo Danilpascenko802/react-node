9f9dd398f150bf830cc393b1d33dbda3c85d2285
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2022-10-14 12:43:58 +0300

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React from "react";
 import Header from "./components/header";
-/*import Mainpage from "./components/mainPage";*/
-import {Outlet} from "react-router-dom";
-/*import Registration from "./components/registration"; */
-/*import Masters from "./components/masters";*/
-/*import { BrowserRouter as Router, Routes, Route } from "react-router-dom";*/
-/*import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";*/
-/*import Contacts from "./components/contacts";*/
+import Home from './pages/Home';
+import About from './pages/About';
+import Masters from './pages/Masters';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/masters' element={<Masters/>}/>
+      </Routes>
     </>
   );
 }
 
 export default App;
 
-/*<Route path="/components/contacts.jsx" component={<Contacts />} />*/
 
-/*<>
-    {}
-    <Router>
-      <Switch>
-        <Route exact path="/" component={<Mainpage />} />
-        <Route path="/components/masters.jsx" component={<Masters />} />
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  </> */
--- src/pages/Masters.js
+++ src/pages/Masters.js
@@ -1,7 +1,6 @@
 import React from "react";
-/*import Header from "./header"*/
+
 function Masters () {
-    /*<Header />*/
     return(
         <h1>Maстера</h1>
     )
